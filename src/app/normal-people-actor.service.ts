fa6ec9ff2e52a6db80a5d1de15caa14f16053c42
Sage Lawrence <w0811501@myscc.ca>
2024-01-30 11:45:52 -0500

    Observable method added

--- src/app/normal-people-actor.service.ts
+++ src/app/normal-people-actor.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {IContent} from "../Models/IContent";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NormalPeopleActorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  //method to return an Observable of the IContent array (contentItems) from the mock-content file
+  fetchData(): Observable<IContent[]> {
+    return this.http.get<IContent[]>('data/mock-content.ts');
+  }
 }
