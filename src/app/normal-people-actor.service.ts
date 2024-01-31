79247bd4e94fd0c51faed5ea4818091c999758db
Sage Lawrence <w0811501@myscc.ca>
2024-01-31 10:51:07 -0500

    Changes to service file

--- src/app/normal-people-actor.service.ts
+++ src/app/normal-people-actor.service.ts
@@ -8,11 +8,11 @@ import {HttpClient} from "@angular/common/http";
 })
 export class NormalPeopleActorService {
 
-  constructor(private http: HttpClient) {
+  constructor(private icontent: IContent[]) {
   }
 
   //method to return an Observable of the IContent array (contentItems) from the mock-content file
-  fetchData(): Observable<any> {
-    return this.http.get('./data/mock-content.ts');
+  fetchData(): Observable<IContent[]> {
+    return this.icontent;
   }
 }
