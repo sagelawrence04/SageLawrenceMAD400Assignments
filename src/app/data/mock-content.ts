c3314897c40536f08f100eb97480237f5b508f22
Sage Lawrence <w0811501@myscc.ca>
2024-01-31 10:54:42 -0500

    Big Fixes

--- src/app/data/mock-content.ts
+++ src/app/data/mock-content.ts
@@ -1,6 +1,6 @@
 import {IContent} from "../../Models/IContent";
 
-export const contentItems: IContent[] = [
+export const CONTENTITEMS: IContent[] = [
   {realName: "Daisy Edgar Jones", characterName: "Marianne Sheridan", age: 25, png: "assets/images/marianne.webp"},
   {realName: "Paul Mescal", characterName: "Connell Waldron", age: 27, png: "assets/images/connell.webp"},
   {realName: "Sarah Greene", characterName: "Lorraine Waldron", age: 39, png: "assets/images/lorraine.jpeg"},
--- src/app/normal-people-actor.service.ts
+++ src/app/normal-people-actor.service.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
 import {IContent} from "../Models/IContent";
-import {HttpClient} from "@angular/common/http";
+import {CONTENTITEMS} from "./data/mock-content";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NormalPeopleActorService {
 
-  constructor(private icontent: IContent[]) {
-  }
-
-  //method to return an Observable of the IContent array (contentItems) from the mock-content file
-  fetchData(): Observable<IContent[]> {
-    return this.icontent;
+  getContentItems(): IContent[] {
+    return CONTENTITEMS;
   }
 }
