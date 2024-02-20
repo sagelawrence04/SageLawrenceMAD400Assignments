b7e5efb4e9ac55448369744411102374598f1b74
Sage Lawrence <w0811501@myscc.ca>
2024-02-19 20:15:08 -0500

    App Component Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {ContentListComponent} from "./content-list/content-list.component";
 import {IContent} from "../Models/IContent";
+import {ContentListItemComponent} from "./content-list-item/content-list-item.component";
+import {CONTENTITEMS} from "./data/mock-content";
+import {NormalPeopleActorService} from "./normal-people-actor.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContentListComponent],
+  imports: [CommonModule, RouterOutlet, ContentListComponent, ContentListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+  content: IContent[] = [];
   title = 'SageLawrenceMyFirstAngularApplication';
+  contentItem = new IContent(5, "New Item", "Example Name", 19, "assets/images/connell.webp");
+  constructor(private normalPeopleActorService: NormalPeopleActorService) {
+  }
+  ngOnInit(): void {
+    this.content = this.normalPeopleActorService.getContentItems();
+  }
+
+  updateContent(): void {
+    this.normalPeopleActorService.updateContentItem(this.contentItem);
+    this.content = this.normalPeopleActorService.getContentItems();
+
+  }
 }
--- src/app/normal-people-actor.service.ts
+++ src/app/normal-people-actor.service.ts
@@ -7,8 +7,6 @@ import {first} from "rxjs";
   providedIn: 'root'
 })
 export class NormalPeopleActorService {
-// removed http client code
-// returning Content Items from mock content list - note the change in the naming convention
   getContentItems(): IContent[] {
     return CONTENTITEMS;
   }
